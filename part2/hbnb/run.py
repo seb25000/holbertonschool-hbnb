4226249f0eb947b2c0e721b0d8dc9d7e1ef86be5
seb25000 <seb25000@gmail.com>
2025-03-01 17:09:18 +0100

    run.py

--- part2/hbnb/run.py
+++ part2/hbnb/run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route("/")  # This defines the route for the root URL
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
+
